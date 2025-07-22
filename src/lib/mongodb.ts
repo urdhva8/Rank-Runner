8de00d57fc8b2cf3543ef5b62643446727537a06
Urdhva <urdhva.suggana@gmail.com>
2025-07-22 19:16:49 +0000

    Application error: a server-side exception has occurred while loading ra

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -3,36 +3,32 @@ import { Db, MongoClient } from "mongodb";
 const MONGODB_URI = process.env.MONGODB_URI;
 const MONGODB_DB = process.env.MONGODB_DB || "rankrunner";
 
-let cachedClient: MongoClient | null = null;
-let cachedDb: Db | null = null;
-
 if (!MONGODB_URI) {
   throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
+    "Please define the MONGODB_URI environment variable inside .env or your deployment settings."
   );
 }
 
+let cachedClient: MongoClient | null = null;
+let cachedDb: Db | null = null;
+
 export async function connectToDatabase() {
   if (cachedClient && cachedDb) {
-    console.log("Using cached database instance.");
     return { client: cachedClient, db: cachedDb };
   }
 
-  console.log("No cached instance found. Creating new connection to MongoDB.");
-  
   try {
-    const client = new MongoClient(MONGODB_URI);
+    const client = new MongoClient(MONGODB_URI!);
     await client.connect();
-    console.log("Successfully connected to MongoDB Atlas.");
-
     const db = client.db(MONGODB_DB);
 
     cachedClient = client;
     cachedDb = db;
 
+    console.log("Successfully connected to MongoDB.");
     return { client, db };
-  } catch (error) {
-    console.error("Failed to connect to MongoDB", error);
-    throw new Error("Failed to connect to the database.");
+  } catch (e) {
+    console.error("Failed to connect to MongoDB", e);
+    throw new Error("Failed to connect to database.");
   }
 }
